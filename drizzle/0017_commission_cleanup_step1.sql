fa7a18fbb2b99d865a71b13b92b315969ef58d7d
7788487847 <290802902@qq.com>
2026-08-19 19:55:38 +0800

    Delete drizzle directory

--- drizzle/0017_commission_cleanup_step1.sql
+++
@@ -1,7 +0,0 @@
-DO $$
-BEGIN
-    IF EXISTS(SELECT 1 FROM information_schema.columns WHERE table_name='users' AND column_name='commission_open') THEN
-        UPDATE users SET commission_status = 1 WHERE commission_open = true;
-    END IF;
-END 
-$$;
