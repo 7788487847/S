f2bbb316cbb932a9a08339e9cfb8b0784d3a6bff
7788487847 <290802902@qq.com>
2026-08-19 19:54:28 +0800

    Delete app directory

--- app/robots.ts
+++
@@ -1,1 +0,0 @@
-import type{MetadataRoute}from"next";export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:["/","/u/","/artwork/"],disallow:["/admin","/dashboard","/api/"]},sitemap:"https://lingxi.art/sitemap.xml"}}
